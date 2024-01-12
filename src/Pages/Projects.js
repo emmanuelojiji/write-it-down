1841029650767666204c5cf2881a1e7a38d2b5f7
emmanuelojiji <emmanuelojiji@hotmail.com>
2024-01-12 22:26:57 +0000

    Project cards created
    Grid container added to projects screen

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,10 +5,11 @@ export const Projects = () => {
   return (
     <div className="projects">
       <main className="projects-main">
-        <h2>Your Projects</h2>
-        <p>Create a new project or continue where you left off</p>
+        <h2 className="projects-main-heading">Your Projects</h2>
+        <p className="projects-main-desc">Create a new project or continue where you left off</p>
 
         <div className="project-card-container">
+          <ProjectCard />
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
